65a4009ee68b593ba50cb4d403d3a2d149ea313b
NuclearCheese <nc.omega0+git@gmail.com>
2017-09-22 19:57:51 -0400

    Add slope support to metatiles

--- RabiRibi_Editor/Metatile.cs
+++ RabiRibi_Editor/Metatile.cs
@@ -39,26 +39,37 @@ namespace RabiRibi_Editor
     
     string name;
     
+    int slope;
+    
     // TODO we might need something smarter
     public Metatile()
     {
       layers = new List<Metatile.Metatile_Layer_Info>();
       
-      name = "Blue library block";
+      //name = "Blue library block";
+      name = "Blue library slope down-right";
+      
+      slope = -1;
       
       // TODO default data to start with
-      top_rows = 1;
+      //top_rows = 1;
+      //mid_rows = 1;
+      //bottom_rows = 1;
+      top_rows = 2;
       mid_rows = 1;
-      bottom_rows = 1;
+      bottom_rows = 0;
       
-      left_cols = 1;
-      mid_cols = 1;
-      right_cols = 1;
+      //left_cols = 1;
+      //mid_cols = 1;
+      //right_cols = 1;
+      left_cols = 0;
+      mid_cols = 2;
+      right_cols = 0;
       
       Metatile_Layer_Info m = new Metatile.Metatile_Layer_Info();
       m.uses_selectable_layer = true;
       m.command = CommandStack.CommandType.Write_Layer_0;
-      m.data = new short[3,3];
+      //m.data = new short[3,3];
       //m.data[0,2] = 1641;
       //m.data[1,2] = 1642;
       //m.data[2,2] = 1643;
@@ -68,50 +79,84 @@ namespace RabiRibi_Editor
       //m.data[0,0] = 1577;
       //m.data[1,0] = 1578;
       //m.data[2,0] = 1579;
-      m.data[0,2] = 297;
-      m.data[1,2] = 298;
-      m.data[2,2] = 297;
-      m.data[0,1] = 329;
-      m.data[1,1] = 361;
-      m.data[2,1] = 329;
-      m.data[0,0] = 297;
-      m.data[1,0] = 298;
-      m.data[2,0] = 297;
+      //m.data[0,2] = 297;
+      //m.data[1,2] = 298;
+      //m.data[2,2] = 297;
+      //m.data[0,1] = 329;
+      //m.data[1,1] = 361;
+      //m.data[2,1] = 329;
+      //m.data[0,0] = 297;
+      //m.data[1,0] = 298;
+      //m.data[2,0] = 297;
+      m.data = new short[2,3];
+      m.data[0,0] = 268;
+      m.data[1,0] = 269;
+      m.data[0,1] = 300;
+      m.data[1,1] = 301;
+      m.data[0,2] = 267;
+      m.data[1,2] = 267;
       
-      m.hflip = new bool[3,3];
-      m.hflip[0,0] = false;
-      m.hflip[1,0] = false;
-      m.hflip[2,0] = true;
-      m.hflip[0,1] = false;
-      m.hflip[1,1] = false;
-      m.hflip[2,1] = true;
-      m.hflip[0,2] = false;
-      m.hflip[1,2] = false;
-      m.hflip[2,2] = true;
-      
-      m.vflip = new bool[3,3];
-      m.vflip[0,0] = false;
-      m.vflip[1,0] = false;
-      m.vflip[1,0] = false;
-      m.vflip[0,1] = false;
-      m.vflip[1,1] = false;
-      m.vflip[1,1] = false;
-      m.vflip[0,2] = true;
-      m.vflip[1,2] = true;
-      m.vflip[2,2] = true;
-      layers.Add(m);
-      
-      m = new Metatile.Metatile_Layer_Info();
-      m.data = new short[3,3];
-      m.hflip = new bool[3,3];
-      m.vflip = new bool[3,3];
-      m.uses_selectable_layer = false;
-      m.command = CommandStack.CommandType.Write_Collision;
-      for (int i = 0; i < 3; i++)
+      //m.hflip = new bool[3,3];
+      //m.hflip[0,0] = false;
+      //m.hflip[1,0] = false;
+      //m.hflip[2,0] = true;
+      //m.hflip[0,1] = false;
+      //m.hflip[1,1] = false;
+      //m.hflip[2,1] = true;
+      //m.hflip[0,2] = false;
+      //m.hflip[1,2] = false;
+      //m.hflip[2,2] = true;
+      m.hflip = new bool[2,3];
+      m.vflip = new bool[2,3];
+      for (int i = 0; i < 2; i++)
       {
         for (int j = 0; j < 3; j++)
         {
-          m.data[i,j] = 1;
+          m.hflip[i,j] = false;
+          m.vflip[i,j] = false;
+        }
+      }
+      
+      //m.vflip = new bool[3,3];
+      //m.vflip[0,0] = false;
+      //m.vflip[1,0] = false;
+      //m.vflip[1,0] = false;
+      //m.vflip[0,1] = false;
+      //m.vflip[1,1] = false;
+      //m.vflip[1,1] = false;
+      //m.vflip[0,2] = true;
+      //m.vflip[1,2] = true;
+      //m.vflip[2,2] = true;
+      layers.Add(m);
+      
+      m = new Metatile.Metatile_Layer_Info();
+      //m.data = new short[3,3];
+      //m.hflip = new bool[3,3];
+      //m.vflip = new bool[3,3];
+      m.uses_selectable_layer = false;
+      m.command = CommandStack.CommandType.Write_Collision;
+      //for (int i = 0; i < 3; i++)
+      //{
+      //  for (int j = 0; j < 3; j++)
+      //  {
+      //    m.data[i,j] = 1;
+      //    m.hflip[i,j] = false;
+      //    m.vflip[i,j] = false;
+      //  }
+      //}
+      m.data = new short[2,3];
+      m.data[0,0] = 7;
+      m.data[1,0] = 5;
+      m.data[0,1] = 1;
+      m.data[1,1] = 1;
+      m.data[0,2] = 1;
+      m.data[1,2] = 1;
+      m.hflip = new bool[2,3];
+      m.vflip = new bool[2,3];
+      for (int i = 0; i < 2; i++)
+      {
+        for (int j = 0; j < 3; j++)
+        {
           m.hflip[i,j] = false;
           m.vflip[i,j] = false;
         }
@@ -150,49 +195,39 @@ namespace RabiRibi_Editor
         CommandStack.CommandEntry cmd =
           new CommandStack.CommandEntry(cmd_type, left, right, top, bottom);
         
-        // TODO stuff
+        // Generate a command entry for each layer the metatile effects.
         for (int x = 0; x < cmd.data.GetLength(0);x ++)
         {
           for (int y = 0; y < cmd.data.GetLength(1); y++)
           {
             int data_x;
             int data_y;
-            // TODO calculate actual positions
             
-            //if (x < left_cols)
-            //{
-            //  data_x = x;
-            //}
-            //else if (x >= (cmd.data.GetLength(0) - right_cols))
-            //{
-            //  data_x = (layer.data.GetLength(0) - right_cols) + (x - (cmd.data.GetLength(0) - right_cols));
-            //}
-            //else
-            //{
-            //  int mid_width = layer.data.GetLength(0) - left_cols - right_cols;
-            //  data_x = (x - left_cols) % mid_width + left_cols;
-            //}
             data_x = CalculateDataCoordinate(x, left_cols, right_cols,
                                              cmd.data.GetLength(0), layer.data.GetLength(0));
             
-            //if (y < top_rows)
-            //{
-            //  data_y = y;
-            //}
-            //else if (y >= (cmd.data.GetLength(1) - bottom_rows))
-            //{
-            //  data_y = (layer.data.GetLength(1) - bottom_rows) + (y - (cmd.data.GetLength(1) - bottom_rows));
-            //}
-            //else
-            //{
-            //  int mid_width = layer.data.GetLength(1) - top_rows - bottom_rows;
-            //  data_y = (y - top_rows) % mid_width + top_rows;
-            //}
-            data_y = CalculateDataCoordinate(y, top_rows, bottom_rows,
-                                             cmd.data.GetLength(1), layer.data.GetLength(1));
+            // If we have a sloped top on this metatile, adjust the top of the
+            // current column accordingly
+            int top_y_offset = 0;
+            int mid_width = layer.data.GetLength(0) - left_cols - right_cols;
+            if ((slope < 0) && (x > left_cols))
+            {
+              top_y_offset = (x - left_cols) / mid_width;
+            }
+            else if ((slope > 0) && (x < (cmd.data.GetLength(0) - right_cols)))
+            {
+              int distance_from_right = (cmd.data.GetLength(0) - x) - right_cols;
+              top_y_offset = (distance_from_right - (mid_width - 1)) / mid_width;
+            }
             
-            //data_x = x % 3;
-            //data_y = y % 3;
+            if (y < top_y_offset)
+            {
+              continue;
+            }
+            
+            data_y = CalculateDataCoordinate(y - top_y_offset, top_rows, bottom_rows,
+                                             cmd.data.GetLength(1) - top_y_offset, layer.data.GetLength(1));
+            
             short temp = layer.data[data_x, data_y];
             if (layer.vflip[data_x, data_y])
             {
@@ -205,7 +240,6 @@ namespace RabiRibi_Editor
             cmd.data[x, y] = temp;
           }
         }
-        
         
         results.Add(cmd);
       }
